954e2a6c3d62a3b99956757a37e6128a2c84a7f7
ayana0409 <duongdoanthuan2003@gmail.com>
2025-09-16 12:48:38 +0700

    fix cor

--- PharmaShop.Api/Program.cs
+++ PharmaShop.Api/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("*")
+            builder.WithOrigins("http://localhost:3000", "https://pharmashop-api.onrender.com", "http://26.139.159.129:3000", "http://localhost:3001")
                    .AllowAnyMethod()
                    .AllowAnyHeader()
                    .AllowCredentials();
